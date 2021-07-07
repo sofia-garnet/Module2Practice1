5de189022e5218d714dd62a6ccfba16b9b7bbcb1
AngrySonya <Photographersofi@gmail.com>
2021-07-07 17:31:54 +0300

    add project basics

---
+++ Module2Practice1/Program.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Module2Practice1
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
